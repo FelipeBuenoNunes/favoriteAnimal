224f2069e7d43ac3fe80e85bfa29256ffae4a78f
Felipe Bueno Nunes <felipebuenonunes16@gmail.com>
2021-11-23 09:18:21 -0400

    ultima alteraçao

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const main = document.querySelector("main");
-
+const footer = document.querySelector("footer");
 const headerFrase = [
     "Cachorro",
     ""
@@ -24,7 +24,7 @@ function fillMain(){
     const description = create("p");
     description.innerText = "Especificamente eu me identifico com o Pit Bull mas gosto de todo o tipo de cachorro, cachorro é um mamífero carnívoro da família dos canídeos, subespécie do lobo, e talvez o mais antigo animal domesticado pelo ser humano. Teorias postulam que surgiu do lobo cinzento no continente asiático há mais de 100 000 anos. Ao longo dos séculos, através da domesticação, o ser humano realizou uma seleção artificial dos cães por suas aptidões, características físicas ou tipos de comportamento. O resultado foi uma grande diversidade de raças caninas, as quais variam em pelagem e tamanho dentro de suas próprias raças, atualmente classificadas em diferentes grupos ou categorias. As designações vira-lata (no Brasil) são dadas aos cães sem raça definida ou mestiços descendentes.";
     const obs = create("p");
-    obs.innerText = "Porém com o PitBull e com algumas raças específicas, muitas pessoas possuem um errado julgamento de que essas raças são 'imprevisíveis' ou que são apenas de 'cão guarda' 😂😂, esse julgamento é extremamente errado, pois o comportamento do animal está relacionado a forma como foi criado, portanto até um inofensivo pintcher pode ter sido criado errado e tentar atacar um humano, mas guardada as devidas proporções.";
+    obs.innerText = "Porém com o PitBull e com algumas raças de grande porte específicas, muitas pessoas possuem um errado julgamento de que essas raças são 'imprevisíveis', isso está equivocado, pois o comportamento do animal está relacionado a forma como foi criado.";
     sectionFirst.appendChild(name);
     sectionFirst.appendChild(description);
     sectionFirst.appendChild(obs);
@@ -33,7 +33,7 @@ function fillMain(){
     divVideo.id = "tagVid";
     main.appendChild(divVideo);
     const dog = create("p");
-    dog.innerText = 'Meu "cão de guarda"';
+    dog.innerText = 'Meu cão de guarda';
     divVideo.appendChild(dog);
 
     const vid = create("video");
@@ -44,9 +44,27 @@ function fillMain(){
     vid.appendChild(source);
 }
 
+const links = [
+    "https://tudosobrecachorros.com.br/",
+    "https://www.petz.com.br/cachorro/racas/pitbull/",
+    "https://www.petlove.com.br/pitbull/r"
+];
+fillFooter()
+function fillFooter(){
+    const p = create("p");
+    p.innerText = "Links"
+    footer.appendChild(p);
+    const div = create("div");
+    footer.appendChild(div);
+    links.forEach(function(item, index){
+    let element = create("a");
+    element.innerText = item;
+    element.href = item;
+    div.appendChild(element);
+    });
+}
+
 //Função com o objetivo de criar o elemento
 function create(element) {
     return document.createElement(element);
 }
-
-//https://tudosobrecachorros.com.br/
